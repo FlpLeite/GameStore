3ad1b7075a709544ad66e34f8dcb3862b5ee5c50
Felipe Leite <122901978+FlpLeite@users.noreply.github.com>
2025-06-27 22:38:14 -0300

    Fix NHibernate helper reference and add POST endpoint

--- back-end/GameStoreAPI/Program.cs
+++ back-end/GameStoreAPI/Program.cs
@@ -5,7 +5,7 @@ using GameStore.Core.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 var connString = builder.Configuration.GetConnectionString("Default");
-builder.Services.AddSingleton(_ => NhHelper.BuildSessionFactory(connString));
+builder.Services.AddSingleton(_ => NHibernate.BuildSessionFactory(connString));
 builder.Services.AddScoped<ISession>(sp => sp.GetRequiredService<ISessionFactory>().OpenSession());
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,6 +19,13 @@ app.MapGet("/games", (ISession session) =>
 {
     var games = session.Query<Game>().ToList();
     return Results.Ok(games);
+});
+
+app.MapPost("/games", (Game game, ISession session) =>
+{
+    session.Save(game);
+    session.Flush();
+    return Results.Created($"/games/{game.Id}", game);
 });
 
 app.Run();
